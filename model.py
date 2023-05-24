02445b39c457c42ef86974313006ce8d7a084064
AntYats <yatsenk2@gmail.com>
2023-05-24 20:38:56 +0300

    model change

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 from sklearn.model_selection import train_test_split
 import pickle
@@ -30,7 +30,7 @@ for image_file in bad_images_files:
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = GradientBoostingClassifier()
+model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
 # Сохранение модели в файл
