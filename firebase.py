fc4a5ceb3126b1e3fdf2aeecf0f71558f52df6f3
Anton <yatsenk2@gmail.com>
2023-05-29 17:58:02 +0300

    Revert "firebase and registration"
    This reverts commit cabbd948464224829403aaf10d482aff5d858847.

--- firebase.py
+++ firebase.py
@@ -27,19 +27,12 @@ def add_liked_to_database(userid, liked_info):
     print("Succesfully added")
     return
 
-def remove_liked_from_database(userid, liked_info):
-    
-
 def get_liked_from_database(userid):
     doc_ref = db.collection('users').document(userid)
     doc = doc_ref.get()
-    
-    if doc.exists:
-        flats = []
-        for key in doc.to_dict():
-            flats = doc.to_dict().get(key)
-            return flats
-    else:
-        data = {'liked_list' : []}
-        doc_ref.set(data)
-        return []
+
+    flats = []
+    for key in doc.to_dict():
+        flats = doc.to_dict().get(key)
+
+    return flats
